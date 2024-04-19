b90e8bfbaf07d7fac6a00fd9fc60b1caab6218a6
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-04-19 01:32:32 +0100

    feat : creacion de la clase Gestor y su atributo

---
+++ src/net/salesianos/gestor/Gestor.java
@@ -0,0 +1,9 @@
+package net.salesianos.gestor;
+
+import java.util.ArrayList;
+
+import net.salesianos.restaurante.Restaurante;
+
+public class Gestor {
+     private static ArrayList<Restaurante> listaRestaurantes = new ArrayList<>();
+}
