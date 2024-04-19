8f301ba538c8bf154b32d6be7baecb14dc37b826
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-04-19 01:50:36 +0100

    feature : Usamos el metodo estatico de mostrar menu en la clase App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import net.salesianos.menu.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu.mostrarMenu();
     }
 }
