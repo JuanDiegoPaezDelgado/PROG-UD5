75df62c7158f47e81869391c5fcca86d67c49896
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-04-19 01:35:00 +0100

    feat : creacion del metodo para poder editar restauranttes este metodo nos permitira editar la informacion de los restaurantes

--- src/net/salesianos/gestor/Gestor.java
+++ src/net/salesianos/gestor/Gestor.java
@@ -7,9 +7,9 @@ import javax.swing.JOptionPane;
 import net.salesianos.restaurante.Restaurante;
 
 public class Gestor {
-     private static ArrayList<Restaurante> listaRestaurantes = new ArrayList<>();
+    private static ArrayList<Restaurante> listaRestaurantes = new ArrayList<>();
 
-     public static void agregarRestaurante() {
+    public static void agregarRestaurante() {
         String nombre = JOptionPane.showInputDialog(null, "Ingrese el nombre del restaurante", "Nombre restaurante",
                 JOptionPane.QUESTION_MESSAGE);
         String localizacion = JOptionPane.showInputDialog(null, "Ingrese la localizacion del restaurante",
@@ -28,4 +28,67 @@ public class Gestor {
 
     }
 
+    public static void editarRestaurante() {
+        String nombreAEditar = JOptionPane.showInputDialog(null, "Ingrese el nombre del restaurante a editar", "Editar",
+                JOptionPane.QUESTION_MESSAGE);
+        for (Restaurante restaurante : listaRestaurantes) {
+            if (restaurante.getNombre().equalsIgnoreCase(nombreAEditar)) {
+                String[] opciones = { "Nombre", "Localizacion", "Horario", "Puntuacion", "Todo" };
+
+                int opcion = JOptionPane.showOptionDialog(null, "Seleccione que desea editar :", "Edicion",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+
+                switch (opcion) {
+                    case 0:
+                        String nuevoNombre = JOptionPane.showInputDialog(null, "Ingrese el nuevo nombre",
+                                "Editar nombre", JOptionPane.QUESTION_MESSAGE);
+                        restaurante.setNombre(nuevoNombre);
+                        break;
+                    case 1:
+                        String nuevaLocalizacion = JOptionPane.showInputDialog(null, "Ingrese la nueva localizacion",
+                                "Editar localizacion", JOptionPane.QUESTION_MESSAGE);
+                        restaurante.setLocalizacion(nuevaLocalizacion);
+                        break;
+                    case 2:
+                        String nuevoHorario = JOptionPane.showInputDialog(null, "Ingrese el nuevo Horario",
+                                "Editar horario", JOptionPane.QUESTION_MESSAGE);
+                        restaurante.setHorario(nuevoHorario);
+                        break;
+                    case 3:
+                        String nuevaPuntuacionText = JOptionPane.showInputDialog(null, "Ingrese la nueva puntuacion",
+                                "Editar puntuacion", JOptionPane.QUESTION_MESSAGE);
+                        float nuevaPuntuacion = Float.parseFloat(nuevaPuntuacionText);
+                        restaurante.setPuntuacion(nuevaPuntuacion);
+                        break;
+                    case 4:
+                        String nuevoNombreTodo = JOptionPane.showInputDialog(null, "Ingrese el nuevo nombre",
+                                "Editar nombre", JOptionPane.QUESTION_MESSAGE);
+                        String nuevaLocalizacionTodo = JOptionPane.showInputDialog(null,
+                                "Ingrese la nueva localizacion",
+                                "Editar localizacion", JOptionPane.QUESTION_MESSAGE);
+                        String nuevoHorarioTodo = JOptionPane.showInputDialog(null, "Ingrese el nuevo Horario",
+                                "Editar horario", JOptionPane.QUESTION_MESSAGE);
+                        String nuevaPuntuacionTextTodo = JOptionPane.showInputDialog(null,
+                                "Ingrese la nueva puntuacion",
+                                "Editar puntuacion", JOptionPane.QUESTION_MESSAGE);
+                        float nuevaPuntuacionTodo = Float.parseFloat(nuevaPuntuacionTextTodo);
+
+                        restaurante.setNombre(nuevoNombreTodo);
+                        restaurante.setLocalizacion(nuevaLocalizacionTodo);
+                        restaurante.setHorario(nuevoHorarioTodo);
+                        restaurante.setPuntuacion(nuevaPuntuacionTodo);
+
+                        break;
+
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opción no válida", "Error", JOptionPane.ERROR_MESSAGE);
+                        break;
+                }
+                JOptionPane.showMessageDialog(null, "Restaurante editado con éxito", "Edición exitosa",
+                        JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+            JOptionPane.showMessageDialog(null, "Restaurante no encontrado", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
